fedc9de036087474f8b5454ed21f9ade1b7c0ca3
javierandresgp <info@javierandres.dev>
2023-05-18 22:12:07 -0500

    javascript

---
+++ javascript/eventos.js
@@ -0,0 +1,8 @@
+'use strict';
+const $saludo = document.getElementById('saludo');
+const $nombre = document.getElementById('nombre');
+const $saludar = document.getElementById('saludar');
+
+console.log($saludo);
+console.log($nombre);
+console.log($saludar);
--- javascript/fundamentos.js
+++ javascript/fundamentos.js
@@ -284,3 +284,126 @@ function mostrarHastaM(m) {
 
 //mostrarHastaM(5);
 //mostrarHastaM(5000);
+
+/* Estructuras de datos */
+// Arreglos, array, vector, lista, matriz
+let arreglo = Array();
+arreglo = [];
+arreglo = [1, 2, 3, 4, 'hola', false, ['a', 'b', 'c']];
+//console.log(arreglo);
+// console.log(typeof arreglo);
+// console.log(arreglo instanceof Object);
+// console.log(arreglo instanceof Array);
+// console.log(arreglo instanceof String);
+//console.log(arreglo[5]);
+//console.log(arreglo[0]);
+//console.log(arreglo.length);
+
+/*
+function longitudArr(arr) {
+  let contador = 0;
+  while (arr[contador] != undefined) {
+    contador++;
+  }
+  console.log(contador);
+}
+longitudArr(arreglo);
+*/
+
+// Objetos
+let objeto = Object();
+objeto = {};
+objeto = {
+  propiedad: 'valor',
+  uno: 1,
+  dos: 2,
+  tres: 3,
+  arr: ['a', 'b'],
+  completado: true,
+  persona: {
+    nombre: 'pepita',
+    apellido: 'pérez',
+  },
+  met1: () => {
+    let txt = 'soy un método';
+    console.log(txt);
+  },
+};
+//console.log(objeto);
+// console.log(typeof objeto);
+// console.log(objeto instanceof Object);
+//console.log(objeto.tres);
+//console.log(objeto['completado']);
+//objeto.met1();
+
+/* Destructuración */
+let perfiles = ['administrador', 'secretario', 'vendedor', 'cliente'];
+//console.log(perfiles);
+const [perfilAdmin, perfilSecre] = perfiles;
+//console.log(perfilAdmin, perfilSecre);
+
+perfiles = {
+  perfil1: 'administrador',
+  perfil2: 'secretario',
+  perfil3: 'vendedor',
+  perfil4: 'cliente',
+};
+//console.log(perfiles);
+const { perfil2, perfil3 } = perfiles;
+//console.log(perfil2, perfil3);
+
+/* Operador Rest */
+function mostrarLista(...lista) {
+  const list = lista;
+  console.log(list);
+  const [nTienda] = list;
+  console.log(nTienda);
+}
+//mostrarLista('tienda doña pepa', 1, 2, 3, 4);
+//mostrarLista('tienda don pepe', 'a', 'e', 'i', 'o', 'u');
+
+const multiplicar = (multiplicador, ...numeros) =>
+  numeros.map((numero) => numero * multiplicador);
+resultado = multiplicar(3, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//console.log(resultado);
+resultado = multiplicar(5, 123, 124, 125);
+//console.log(resultado);
+
+function mostrarTotal(...valores) {
+  let total = 0;
+  for (const i of valores) {
+    total += i;
+  }
+  console.log(total);
+}
+//mostrarTotal(100, 150, 250);
+//mostrarTotal(1000, 150, 250);
+
+/* Operador Spread */
+const empleado = {
+  nombre: 'pepita',
+  apellido: 'pérez',
+  cargo: 'vendedor',
+  activo: true,
+  nHijos: 2,
+};
+const copiaEmpleado = empleado;
+const copiadoEmpleado = { ...empleado };
+empleado.nHijos = 3;
+empleado.email = 'ej@ejemplo.com';
+//console.log(empleado);
+//console.log(copiaEmpleado);
+//console.log(copiadoEmpleado);
+
+const objA = {
+  a: 'a',
+  b: 'b',
+};
+const objB = {
+  c: 'c',
+  d: 'd',
+};
+const objC = { objA, objB };
+//console.log(objC);
+const objD = { ...objA, ...objB };
+//console.log(objD);
