9b611f3d791473197ed0c4c90e809c33e80882ac
javierandresgp <info@javierandres.dev>
2023-05-19 19:41:16 -0500

    javascript

--- javascript/eventos.js
+++ javascript/eventos.js
@@ -1,8 +1,23 @@
 'use strict';
 const $saludo = document.getElementById('saludo');
-const $nombre = document.getElementById('nombre');
 const $saludar = document.getElementById('saludar');
 
-console.log($saludo);
-console.log($nombre);
-console.log($saludar);
+const formulario = {
+  nombre: null,
+};
+
+$saludar.nombre.addEventListener('input', (evento) => {
+  formulario.nombre = evento.target.value;
+});
+
+$saludar.addEventListener('submit', (evento) => {
+  evento.preventDefault();
+  if (formulario.nombre != null) {
+    $saludo.style.color = 'green';
+    $saludo.textContent = `Hola, ${formulario.nombre}!`;
+    $saludar.reset();
+  } else {
+    $saludo.style.color = 'red';
+    $saludo.textContent = 'Por favor ingresa tu nombre.';
+  }
+});
